e0d0635e5147335ebfea5470e9e0ed2c0fab29ca
Peng Guo <guoleii@gmail.com>
2014-11-24 15:53:27 +0800

    Initial Commit

---
+++ PageWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageWatch
+//
+//  Created by Peng on 11/24/14.
+//  Copyright (c) 2014 peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PageWatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PageWatch
+//
+//  Created by Peng on 11/24/14.
+//  Copyright (c) 2014 peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
